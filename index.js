a754aead5a4e7743244ecc628f3421cd69faa04f
JSpeed8th <jspeed8th@gmail.com>
2018-01-03 05:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function lowerCaseDrivers (array) {
 function nameToAttributes (names) {
   return names.map(function (name) {
     const splitNames = name.split(" ");
-    return name.firstName = splitNames[0];
+    console.log (name.firstName = splitNames[0]);
     console.log (name.firstName = splitNames[1]);
+    
   })
 };
