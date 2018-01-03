87a1da73cd9a84eb8071787b3f01a936d988dae4
JSpeed8th <jspeed8th@gmail.com>
2018-01-03 05:27:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ function nameToAttributes (names) {
     const splitNames = name.split(" ");
     console.log (splitNames[0])
     console.log(splitNames[1])
+    console.log name.firstName = "Ya";
   })
 };
